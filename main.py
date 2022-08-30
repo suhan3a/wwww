89f9287de89560e794e3df2d6d4695677056e611
suhan3a <112479546+suhan3a@users.noreply.github.com>
2022-08-30 20:20:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     love_day = int(config["love_date"].split("-")[2])
     love_date = date(love_year, love_month, love_day)
     # 获取在一起的日期差
-    love_days = str(today.__sub__(love_date)).split(" ")[0]
+    love_days = str(Yesterday.__sub__(love_date)).split(" ")[0]
     # 获取所有生日数据
     birthdays = {}
     for k, v in config.items():
